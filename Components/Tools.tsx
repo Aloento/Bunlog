15c1ea9906261f7ee9ed996ba1e3e7f415e9dfdb
Aloento <11802769+Aloento@users.noreply.github.com>
2023-04-30 15:31:03 +0200

    html

--- Components/Tools.tsx
+++ Components/Tools.tsx
@@ -20,7 +20,29 @@ export function Tools() {
           <Button icon={<DeleteRegular />} appearance="subtle" />
         </Admin>
 
-        <Button icon={<DocumentPdfRegular />} appearance="subtle" />
+        <Button icon={<DocumentPdfRegular />} appearance="subtle" onClick={() => {
+          var s = new XMLSerializer();
+          var html = s.serializeToString(document.querySelector(".LexEditor_Shell")!);
+
+          const a = document.createElement('a');
+          const body = document.body;
+          body.appendChild(a);
+          a.style.display = 'none';
+
+          const url = window.URL.createObjectURL(
+            new Blob(
+              [JSON.stringify(html)], {
+              type: 'octet/stream'
+            })
+          );
+
+          a.href = url;
+          a.download = `Bunlog ${new Date().toISOString()}.html`;
+          a.click();
+          window.URL.revokeObjectURL(url);
+          a.remove();
+        }} />
+
         <Button icon={<DocumentJavascriptRegular />} appearance="subtle" onClick={() => {
           exportFile(CurrentEditor!, {
             fileName: `Bunlog ${new Date().toISOString()}`,
