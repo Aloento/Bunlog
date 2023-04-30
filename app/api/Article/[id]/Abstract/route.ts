968552b1ae8466a1f2d2b36c33c304ced06a96e7
Aloento <11802769+Aloento@users.noreply.github.com>
2023-04-30 19:40:33 +0200

    FIN

--- app/api/Article/[id]/Abstract/route.ts
+++ app/api/Article/[id]/Abstract/route.ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/app/api";
+
 /**
  * 
  *
@@ -6,6 +8,6 @@
  * @version 0.1.0
  */
 export async function GET(request: Request, { params: { id } }: { params: { id: string } }) {
-  const post = "Modern web technologies provide ample ways to work with video. Media Stream API, Media Recording API, Media Source API, and WebRTC API add up to a rich tool set for recording, transferring, and playing video streams. While solving certain high-level tasks, these APIs don't let web programmers work with individual components of a video stream such as frames and unmuxed chunks of encoded video or audio. To get low-level access to these basic components, developers have been using WebAssembly to bring video and audio codecs into the browser. But given that modern browsers already ship with a variety of codecs (which are often accelerated by hardware), repackaging them as WebAssembly seems like a waste of human and computer resources."
-  return new Response(post);
+  const { abstract } = await prisma.post.findFirstOrThrow({ where: { id: parseInt(id) }, select: { abstract: true } });
+  return new Response(abstract);
 }
