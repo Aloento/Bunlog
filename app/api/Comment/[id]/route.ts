153b7639ea609f8875ce2170c37839e4311c7141
Aloento <11802769+Aloento@users.noreply.github.com>
2023-04-30 18:30:55 +0200

    IComment

--- app/api/Comment/[id]/route.ts
+++ app/api/Comment/[id]/route.ts
@@ -1,4 +1,6 @@
 import { IComment } from "@/app/Article/[id]/Comment";
+import { getServerSession } from "next-auth";
+import { prisma } from "../..";
 
 /**
  * 
@@ -8,11 +10,24 @@ import { IComment } from "@/app/Article/[id]/Comment";
  * @version 0.1.0
  */
 export async function GET(request: Request, { params: { id } }: { params: { id: string } }) {
+  const { userName, posted, content } = await prisma.comment.findFirstOrThrow({
+    where: {
+      id: parseInt(id)
+    }
+  })
+
+  const { email } = await prisma.user.findFirstOrThrow({
+    where: {
+      name: userName
+    },
+    select: { email: true }
+  })
+
   const res: IComment = {
-    Name: "Aloento",
-    EMail: "Aloento@Q-Audio.org",
-    Posted: new Date(),
-    Content: "这是一串非常长的文本这是一串非常长的文本这是一串非常长的文本这是一串非常长的文本这是一串非常长的文本这是一串非常长的文本"
+    Name: userName,
+    EMail: email,
+    Posted: posted,
+    Content: content
   };
 
   return new Response(JSON.stringify(res));
@@ -25,7 +40,17 @@ export async function GET(request: Request, { params: { id } }: { params: { id: 
  * @since 0.1.0
  * @version 0.1.0
  */
-export async function POST(request: Request) {
+export async function POST(request: Request, { params: { id } }: { params: { id: string } }) {
+  const s = await getServerSession();
+
+  await prisma.comment.create({
+    data: {
+      content: await request.text(),
+      postId: parseInt(id),
+      userName: s!.user!.name!
+    }
+  })
+
   return new Response(null, {
     status: 201
   })
